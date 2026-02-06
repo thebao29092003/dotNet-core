a138a8b90a8c461a47ea7ba5d2d7597b72edf1db
bao <thebao29092003@gmail.com>
2026-02-06 11:02:25 +0700

    feat: xong bai 1,2 setup and model

---
+++ Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace coreC_.Models
+{
+    public class Stock
+    {
+        public int ID { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Divdend { get; set; }
+
+        public decimal LastDiv {get; set;}
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
